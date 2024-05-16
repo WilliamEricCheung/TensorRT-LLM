3aec0ded2bd7186bc5e3ad33bee932a78c22005f
Yihui Zhang <zhangyihui@buaa.edu.cn>
2024-05-16 14:34:21 +0800

    Update docker build scripts by bypassing proxy

--- docker/common/install_cmake.sh
+++ docker/common/install_cmake.sh
@@ -8,7 +8,12 @@ CMAKE_VERSION="3.24.4"
 PARSED_CMAKE_VERSION=$(echo $CMAKE_VERSION | sed 's/\.[0-9]*$//')
 CMAKE_FILE_NAME="cmake-${CMAKE_VERSION}-linux-${ARCH}"
 RELEASE_URL_CMAKE=https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_FILE_NAME}.tar.gz
-wget --no-verbose ${RELEASE_URL_CMAKE} -P /tmp
+
+# 设置代理服务器地址
+PROXY_SERVER="http://127.0.0.1:7890"
+
+# 使用代理下载文件
+http_proxy=${PROXY_SERVER} https_proxy=${PROXY_SERVER} wget --no-verbose ${RELEASE_URL_CMAKE} -P /tmp
 tar -xf /tmp/${CMAKE_FILE_NAME}.tar.gz -C /usr/local/
 ln -s /usr/local/${CMAKE_FILE_NAME} /usr/local/cmake
 
