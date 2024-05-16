2250dc9e2820b595ba25c631b39c1a570e0dfaf7
Yihui Zhang <zhangyihui@buaa.edu.cn>
2024-05-16 16:39:10 +0800

    Update docker build scripts by bypassing proxy

--- docker/common/install_ccache.sh
+++ docker/common/install_ccache.sh
@@ -6,7 +6,10 @@ ARCH=$(uname -m)
 CCACHE_VERSION="4.8.3"
 SYSTEM_ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
 
+# 设置代理服务器地址
+PROXY_SERVER="http://127.0.0.1:7890"
+
 if [[ $ARCH == *"x86_64"* ]] && [[ $SYSTEM_ID == *"centos"* ]]; then
-  curl -L https://github.com/ccache/ccache/releases/download/v${CCACHE_VERSION}/ccache-${CCACHE_VERSION}-linux-${ARCH}.tar.xz | xz -d | tar -x -C /tmp/
+  http_proxy=${PROXY_SERVER} https_proxy=${PROXY_SERVER} curl -L https://github.com/ccache/ccache/releases/download/v${CCACHE_VERSION}/ccache-${CCACHE_VERSION}-linux-${ARCH}.tar.xz | xz -d | tar -x -C /tmp/
   cp /tmp/ccache-${CCACHE_VERSION}-linux-x86_64/ccache /usr/bin/ccache
 fi
